2b906000cd781db2d77e6fdf1035eb35ae11f613
audreyandoy <audreylandoy@gmail.com>
2016-04-08 10:25:15 -0700

    facebook link bigger

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -14,7 +14,7 @@ const Login = React.createClass({
          <div class="jumbotron text-center">
             <h1><span class="fa fa-lock"></span> Node Authentication</h1>
 
-            <p>Login or Register with:</p>
+            <h3><p>Login or Register with:</p></h3>
 
             <a href="/auth/facebook" class="btn btn-primary"><span class="fa fa-facebook"></span> Facebook</a>
 
